0092a898953c8c08dbb3814a4d6318bdecdef663
james-koduah <jameskoduah171@gmail.com>
2024-01-24 17:58:03 +0000

    app.py: task 13: second

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def login():
 @app.route('/sessions', methods=['delete'], strict_slashes=False)
 def logout():
     """log a user out and destroy session"""
-    session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get('session_id', '')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
