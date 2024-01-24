aaba136a4558630594e62f62a305daa9b1659fb6
james-koduah <jameskoduah171@gmail.com>
2024-01-24 14:45:44 +0000

    auth.py: task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,26 @@
 """authentication mehtods"""
 
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register a user"""
+        try:
+            self._db.find_user_by(email=email)
+        except Exception as e:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+        raise ValueError(f'User {email} already exists')
 
 
 def _hash_password(password: str) -> bytes:
