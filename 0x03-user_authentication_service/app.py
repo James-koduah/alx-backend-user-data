ec5e0974c1825b65c30a30e541104e1497e70d80
james-koduah <jameskoduah171@gmail.com>
2024-01-24 15:02:27 +0000

    app.py: task 7: pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,5 +29,6 @@ def users():
     except Exception as e:
         return jsonify({"message": "email already registered"}), 400
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
