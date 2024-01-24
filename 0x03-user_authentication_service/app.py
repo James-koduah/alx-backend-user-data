95feba3bd001cd7121d0ec433fac23bbc38f84b8
james-koduah <jameskoduah171@gmail.com>
2024-01-24 18:57:31 +0000

    app.py: task 19: third: wrong variable

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def update_password():
     new_password = request.form.get('new_password')
 
     try:
-        AUTH.update_password(reset_token, password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": f"{email}", "message": "Password updated"})
     except Exception as e:
         abort(403)
