ff926a224fac8409465761bef8ce2efa1e0f88c6
james-koduah <jameskoduah171@gmail.com>
2024-01-24 16:48:54 +0000

    auth.py: task 9: type annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,6 +42,6 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """return a string representation of a new UUID"""
     return str(uuid.uuid4())
